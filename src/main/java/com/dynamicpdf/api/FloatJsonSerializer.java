49629ab7ea0d3b66f21ccd10aebf1280b4b70353
Kashinath G. S <kashinath@dynamicpdf.com>
2023-07-18 15:50:08 +0530

    XML comment updated.

--- src/main/java/com/dynamicpdf/api/FloatJsonSerializer.java
+++ src/main/java/com/dynamicpdf/api/FloatJsonSerializer.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
+/**
+ * <p><b>This class is meant for DynamicPDF's internal usage and not supposed to be used by the client programs.</b></p>
+ */
 public class FloatJsonSerializer extends JsonSerializer<Float>{
 
 	@Override
